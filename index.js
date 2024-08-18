a9f76ec92ffdb43b3aaa9b3548caa56ab8059a3d
Dhananjaya Sri Deshan <dsrideshan@gmail.com>
2024-08-18 08:23:51 +0530

    animation added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ hamburger.addEventListener('click', mobileMenu);
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
     body.classList.toggle("nav-open");
 }
 
@@ -23,3 +22,31 @@ function closeMenu() {
 }
 
 // end navbar seciotn //
+
+// scroll animation //
+
+const the_animation = document.querySelectorAll('.animation, .animation-left, .animation-right');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=> {
+        if (entry.isIntersecting){
+            entry.target.classList.add('scroll-animation');
+        }
+        else {
+            entry.target.classList.remove('scroll-animation');
+
+        }
+
+        
+    })
+});
+
+console.log(the_animation);
+
+for (let i =0; i < the_animation.length; i++){
+    const elements = the_animation[i];
+
+    observer.observe(elements);
+
+
+}
